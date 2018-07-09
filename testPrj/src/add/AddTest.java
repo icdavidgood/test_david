ca6897ce2d77baa59ce860207555c5485a5ecc48
icdavidgood <icdavidgood@gmail.com>
2018-07-09 23:45:13 +0900

    Update AddTest.java

--- testPrj/src/add/AddTest.java
+++ testPrj/src/add/AddTest.java
@@ -5,6 +5,12 @@ public class AddTest {
 	public static void main(String[] args) {
 		
 		System.out.println("test case22");
+		
+		System.out.println("test case22");
+		
+		System.out.println("test case22");
+		
+		System.out.println("test case22");
 
 	}
 
